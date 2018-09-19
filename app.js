d71cb9501034c1d0218e2c9e295f11d6fc1a7e9c
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-09-19 13:39:23 +0900

    Remove middleware

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const awsServerlessExpress = require('aws-serverless-express');
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 
 const app = express();
-app.use(awsServerlessExpressMiddleware.eventContext())
 
 app.get('/', (req, res) => {
   res.json({
-    e: req.apiGateway.event,
     message: 'Hello Home!',
   });
 });
 
 app.get('/child', (req, res) => {
   res.json({
-    e: req.apiGateway.event,
     message: 'Hello Child!',
   });
 });
