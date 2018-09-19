149123d7261867932a9fda948b234228262e9ace
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-09-19 13:31:18 +0900

    Use Api Gateway middleware

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const awsServerlessExpress = require('aws-serverless-express');
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+app.get('/', (req, res) => {
+  res.json({
+    e: req.apiGateway.event,
+    message: 'Hello Home!',
+  });
+});
+
+app.get('/child', (req, res) => {
+  res.json({
+    e: req.apiGateway.event,
+    message: 'Hello Child!',
+  });
+});
 
 const server = awsServerlessExpress.createServer(app);
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
