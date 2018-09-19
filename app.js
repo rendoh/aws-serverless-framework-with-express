015c9e0bc810f0d8846443c2ef83691a8982ebee
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-09-19 13:15:58 +0900

    Add aws-serverless-express

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const express = require('express');
+const awsServerlessExpress = require('aws-serverless-express');
+
 const app = express();
 app.get('/', (req, res) => res.send('Hello World!'));
+
+const server = awsServerlessExpress.createServer(app);
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
